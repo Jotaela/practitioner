e64ea14df259af641a32cc1792212e2b99fec922
jotaela <joanlluisfabra@gmail.com>
2019-09-23 19:37:31 +0200

    WIP

--- src/app.js
+++ src/app.js
@@ -1,12 +1,6 @@
-//console.log('keloke')
- 
 const name="joan"
 
-document.body.innerHTML= template
-
-//document.body.innerHTML = '<h1>hola ' + name +'</h1>'
-
-//import i export
- import salutacions from './js/templates/salutacions.js.js'
- console.log(salutacions);
  
+import salutacions from './js/templates/salutacions.js'
+
+document.body.innerHTML= salutacions
